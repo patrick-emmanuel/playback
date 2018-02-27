e1f1af847fba5528717b7c09dcbaaf184840939e
Patrick <inem.patrick@gmail.com>
2018-02-27 11:58:55 +0100

    Add home service

--- src/containers/HomePage/service.js
+++ src/containers/HomePage/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const appCred = {
   id: 271642,
-  redirectUrl: 'https://tolocalhost.com/',
+  redirectUrl: 'https://dizur.herokuapp.com',
   responseType: 'token'
 };
 const deezerAuthUrl = `https://cors-anywhere.herokuapp.com/https://connect.deezer.com/oauth/auth.php?app_id=${appCred.id}&redirect_uri=${appCred.redirectUrl}&response_type=${appCred.responseType}`;
