97c6998cc85271c2527d3f2cfb0d456857634c9a
Patrick <inem.patrick@gmail.com>
2018-04-01 17:04:30 +0100

    Make search bar flat

--- src/components/Chart/ChartImage.js
+++ src/components/Chart/ChartImage.js
@@ -1,9 +1,7 @@
 import Image from '../Image';
 
 const ChartImage = Image.extend`
-  &:hover {
-    opacity: 0.5;
-  }
+
 `;
 
 export default ChartImage;
--- src/components/SearchBar/Input.js
+++ src/components/SearchBar/Input.js
@@ -4,7 +4,6 @@ const Input = styled.input`
   width: 100%;
   height: 38px;
   background: #bdb5b5;
-  border-radius: 25px;
   border: none;
   font-family: Ubuntu;
   padding-left: 20px;
